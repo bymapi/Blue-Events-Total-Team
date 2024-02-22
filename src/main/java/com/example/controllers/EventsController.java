8eabc8c021c09a972430f1b944cd8d9b97a9792f
bymapi <mapihernandezc@gmail.com>
2024-02-22 15:09:22 +0100

    EventsDao created MVP JpaRepository

--- src/main/java/com/example/controllers/EventsController.java
+++ src/main/java/com/example/controllers/EventsController.java
@@ -1,5 +1,16 @@
 package com.example.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.dao.EventsDao;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
 public class EventsController {
+    private final EventsDao eventsDao;
 
 }
