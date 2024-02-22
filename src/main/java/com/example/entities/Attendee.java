469364df9a2c9a5c263b89423755bd0b4f9bf8e2
oumaymaBombarek <total.oumayma82@gmail.com>
2024-02-22 15:09:32 +0100

    EntidadesHechas

--- src/main/java/com/example/entities/Attendee.java
+++ src/main/java/com/example/entities/Attendee.java
@@ -1,5 +1,68 @@
 package com.example.entities;
 
-public class Attendee {
+import java.io.Serializable;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "attendees")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Attendee implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotEmpty(message = "The name cannot be empty")
+    @Pattern(regexp = "^[a-zA-Z]+$", message = "Write alphabetic letters only ")
+    private String name;
+
+    @NotEmpty(message = "The surname cannot be empty")
+    @Pattern(regexp = "^[a-zA-Z]+$", message = "Write alphabetic letters only ")
+    private String surname;
+
+    // @Size(min = 5, max = 9, message = "the number of characters of this id cannot
+    // be less than 5 or more than 9")
+    private int globalId;
+
+    @NotEmpty(message = "The field mail cannot be empty")
+    @Pattern(regexp = "^[a-zA-Z0-9_.+-]+@blue\\.com$", message = "The mail should have the address @blue.com")
+    private String mail;
+
+    @NotEmpty(message = "The profile cannot be empty")
+    private Options options;
+
+    @JsonIgnore
+     @ManyToMany(fetch = FetchType.EAGER,
+      cascade = {
+          CascadeType.PERSIST,
+          CascadeType.MERGE
+      },
+      mappedBy = "attendees")
+    private List<Event> usuarios;
 
 }
--- src/main/java/com/example/entities/Event.java
+++ src/main/java/com/example/entities/Event.java
@@ -1,5 +1,103 @@
 package com.example.entities;
 
-public class Event {
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+import org.hibernate.validator.constraints.UniqueElements;
+import org.springframework.format.annotation.DateTimeFormat;
+
+
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+@Table(name = "events")
+
+public class Event implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @UniqueElements
+    private int idGlobal;
+
+    @NotNull(message = "Must not be empty")
+    @Pattern(regexp = "^[a-zA-Z]+$", message = "Write alphabetic letters only ")
+    private String title;
+
+    @NotNull
+    private Target target;
+
+    @NotNull(message = "Must not be empty")
+    @Pattern(regexp = "^[a-zA-Z]+$", message = "Write alphabetic letters only ")
+    private String description;
+
+    @NotNull(message = "Must not be empty")
+    @DateTimeFormat(pattern = "dd-MM-YY")
+    private LocalDate startDate;
+
+    @NotNull(message = "Must not be empty")
+    @DateTimeFormat(pattern = "dd-MM-YY")
+    private LocalDate endDate;
+
+    @NotNull(message = "Must not be empty")
+    @DateTimeFormat(pattern = "HH:mm")
+    private LocalTime startTime;
+
+    @NotNull(message = "Must not be empty")
+    @DateTimeFormat(pattern = "HH:mm")
+    private LocalTime endTime;
+    
+
+    @NotNull(message = "Must not be empty")
+    private Mode mode;
+
+    // A preguntar pero de momento lo pongo como String
+    @NotNull(message = "Must not be empty")
+    private String place;
+
+    @ManyToMany(fetch = FetchType.EAGER,
+    cascade = {CascadeType.PERSIST,
+        CascadeType.MERGE})
+  
+   @JoinTable(name = "events_attendees",
+       joinColumns = { @JoinColumn(name = "id_event", referencedColumnName = "id_event") },
+       inverseJoinColumns = { @JoinColumn(name = "id_attendee", referencedColumnName = "id_attendee") })
+
+       private List<Attendee> attendees;
+  
+   
+
+
 
 }
