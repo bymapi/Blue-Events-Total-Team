f3c69b1bf5d0375c3820076200c1947feaeb0e76
bymapi <mapihernandezc@gmail.com>
2024-03-06 15:52:15 +0100

    US Branch created + Entities

--- src/main/java/com/example/entities/Attendee.java
+++ src/main/java/com/example/entities/Attendee.java
@@ -1,5 +1,77 @@
 package com.example.entities;
 
-public class Attendee {
+import java.io.Serializable;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+@Entity
+@Table(name = "attendees")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class Attendee implements Serializable {
+
+        private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotEmpty(message = "The name cannot be empty")
+    private String name;
+
+    @NotEmpty(message = "The surname cannot be empty")
+    private String surname;
+
+    @Column(unique = true)
+    @Min(value = 10000, message = "The globalId must be greater than or equal to 10000")
+    @Max(value = 999999999, message = " The globalId must be less than or equal to 999999999")
+    private int globalId;
+
+    @NotEmpty(message = "The field mail cannot be empty")
+    @Pattern(regexp = "^[a-zA-Z0-9_.+-]+@blue\\.com$", message = "The mail should have the address @blue.com")
+    private String mail;
+
+
+    @NotNull(message = "The profile cannot be empty")
+    private Profile profile;
+
+    @JsonIgnore
+     @ManyToMany(fetch = FetchType.EAGER,
+      cascade = {
+          CascadeType.PERSIST,
+          CascadeType.MERGE
+      },
+      mappedBy = "attendees")
+      
+    private Set<Event> events;
+
+   
 
 }
--- src/main/java/com/example/entities/Event.java
+++ src/main/java/com/example/entities/Event.java
@@ -1,7 +1,106 @@
 package com.example.entities;
 
-public class Event {
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.HashSet;
 
-    // update from temporals
+import java.util.Set;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+@Table(name = "events")
+public class Event implements Serializable {
+
+        private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotNull(message = "Must not be empty")
+    private String title;
+
+    @NotNull
+    private Target target;
+
+    @NotNull(message = "Must not be empty")
+    private String description;
+
+    @NotNull(message = "Must not be empty")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate startDate;
+
+    @NotNull(message = "Must not be empty")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate endDate;
+
+    @NotNull(message = "Must not be empty")
+    @DateTimeFormat(pattern = "HH:mm")
+    private LocalTime startTime;
+
+    @NotNull(message = "Must not be empty")
+    @DateTimeFormat(pattern = "HH:mm")
+    private LocalTime endTime;
+
+    @NotNull(message = "Must not be empty")   
+    private EventStatus eventStatus;
+
+    @NotNull(message = "Must not be empty")
+    private Mode mode;
+
+    @NotNull(message = "Must not be empty")
+    private String place;
+
+
+    private final int maximumNumberOfAttendees = 8;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = { CascadeType.PERSIST,
+            CascadeType.MERGE })
+    @JoinTable(name = "events_attendees", joinColumns = {
+            @JoinColumn(name = "id_event") }, inverseJoinColumns = {
+                    @JoinColumn(name = "id_attendee") })
+    private Set<Attendee> attendees = new HashSet<>();
+
+
+
+    public void addAttendees(Attendee attendee) {
+        this.attendees.add(attendee);
+        attendee.getEvents().add(this);
+    }
+
+    public void removeAttendee(int attendeeId) {
+        Attendee attendee = this.attendees.stream().filter(e -> e.getId() == attendeeId).findFirst().orElse(null);
+        if (attendee != null) {
+            this.attendees.remove(attendee);
+            attendee.getEvents().remove(this);
+
+        }
+    }
 
 }
