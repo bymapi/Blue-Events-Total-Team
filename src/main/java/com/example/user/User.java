bbc719ba6454a899070fad09c57932366786dcdb
bymapi <mapihernandezc@gmail.com>
2024-03-07 17:57:31 +0100

    security added

---
+++ src/main/java/com/example/user/User.java
@@ -0,0 +1,44 @@
+package com.example.user;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "_users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    @Column(unique = true)
+    @Pattern(regexp = "^[a-zA-Z0-9_.+-]+@blue\\.com$", message = "The mail should have the address @blue.com")
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+}
---
+++ src/main/java/com/example/user/UserService.java
@@ -0,0 +1,16 @@
+package com.example.user;
+
+import java.util.List;
+
+public interface UserService {
+    public abstract User add(User user);
+
+    public abstract List<User> findAll();
+
+    public abstract void deleteByEmail(String email);
+
+    public abstract User findByEmail(String email);
+
+    public abstract User update(User user);
+
+}
