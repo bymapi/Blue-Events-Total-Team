764a6a6afe74914c00e290d82e0a8469b39af450
oumaymaBombarek <total.oumayma82@gmail.com>
2024-03-07 17:48:04 +0100

    Evento con foto hecho

--- src/main/java/com/example/controllers/EventsController.java
+++ src/main/java/com/example/controllers/EventsController.java
@@ -1,5 +1,6 @@
 package com.example.controllers;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
@@ -11,6 +12,7 @@ import java.util.Map;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -21,10 +23,15 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.example.entities.Event;
 import com.example.entities.EventDTOAdmin;
+import com.example.helpers.FileDownload;
+import com.example.helpers.FileUpLoad;
+import com.example.model.FileUploadResponse;
 import com.example.services.EventsService;
 
 import jakarta.validation.Valid;
@@ -36,17 +43,44 @@ import lombok.RequiredArgsConstructor;
 public class EventsController {
 
     private final EventsService eventsService;
+     private final FileUpLoad fileUpLoad;
+    private final FileDownload fileDownload;
 
     /*
      * US 1.2. Create a new internal event.
      * As Administrator I want to create new internal events.
      */
-    @PostMapping("/events")
-    public ResponseEntity<Map<String, Object>> createEvent(@Valid @RequestBody Event event,
-            BindingResult validationResults) {
 
-        Map<String, Object> responseAsMap = new HashMap<>();
-        ResponseEntity<Map<String, Object>> responseEntity = null;
+    @PostMapping(consumes = "multipart/form-data")
+    @Transactional
+    public ResponseEntity<Map<String,Object>> createEvent(@Valid @RequestPart(name = "event", required = true) Event event,     
+    BindingResult validationResults,
+                @RequestPart(name = "file", required = false) MultipartFile file) {
+
+        Map<String,Object> responseAsMap = new HashMap<>();
+        ResponseEntity<Map<String,Object>> responseEntity = null;
+
+        if (file != null) {
+
+            try {
+                String fileName = file.getOriginalFilename();
+                String fileCode = fileUpLoad.saveFile(fileName, file);
+                event.setImagen(fileCode + "-" + fileName) ;
+
+                 FileUploadResponse fileUploadResponse = FileUploadResponse
+                       .builder()
+                       .fileName(fileCode + "-" + fileName)
+                       .downloadURI("/event/downloadFile/" 
+                                 + fileCode + "-" + fileName)
+                       .size(file.getSize())
+                       .build();
+            
+            responseAsMap.put("info de la imagen: ", fileUploadResponse);           
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
         // First, we check if the event itself has errors
 
@@ -56,39 +90,40 @@ public class EventsController {
             List<ObjectError> objectErrors = validationResults.getAllErrors();
 
             objectErrors.forEach(objectError -> errors.add(objectError.getDefaultMessage()));
-
-            responseAsMap.put("errors", errors);
+            
+            responseAsMap.put("errors",errors);
             responseAsMap.put("malformed event", event);
 
-            responseEntity = new ResponseEntity<Map<String, Object>>(responseAsMap, HttpStatus.BAD_REQUEST);
+            responseEntity = new ResponseEntity<Map<String,Object>>(responseAsMap, HttpStatus.BAD_REQUEST);
 
             return responseEntity;
 
         }
 
         // As long as the validation goes fine, we can proceed to create our super event
-
+        
         try {
 
             Event eventCreated = eventsService.eventSaved(event);
             String successMessage = "The event was succesfully created";
             responseAsMap.put("Success Message", successMessage);
             responseAsMap.put("Created event", eventCreated);
-            responseEntity = new ResponseEntity<Map<String, Object>>(responseAsMap, HttpStatus.CREATED);
-
+            responseEntity = new ResponseEntity<Map<String,Object>>(responseAsMap, HttpStatus.CREATED);
+            
         } catch (DataAccessException e) {
             String error = "Something went wrong while creating the event and the most specific cause is: "
-                    + e.getMostSpecificCause();
+            + e.getMostSpecificCause();
 
             responseAsMap.put("error", error);
             responseAsMap.put("event that was intended to be created", event);
-            responseEntity = new ResponseEntity<Map<String, Object>>(responseAsMap, HttpStatus.INTERNAL_SERVER_ERROR);
+            responseEntity = new ResponseEntity<Map<String,Object>>(responseAsMap,HttpStatus.INTERNAL_SERVER_ERROR);
         }
-
-        // Enjoy !
+        
+        
         return responseEntity;
     }
 
+   
     /*
      * US 1.3. Modify an event.
      * As Administrator I want to modify or delete an event.
---
+++ src/main/java/com/example/model/FileUploadResponse.java
@@ -0,0 +1,9 @@
+package com.example.model;
+
+import lombok.Builder;
+
+@Builder
+
+public record FileUploadResponse(String fileName, String downloadURI, long size) {
+
+}
